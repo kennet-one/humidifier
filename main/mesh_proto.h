2951e407d11d75e5cfbb017a51c6b09071c4b8e2
kennet-one <kennet1one@gmail.com>
2026-09-20 21:21:39 +0200

    adopt operation-aware control core

--- main/mesh_proto.h
+++ main/mesh_proto.h
@@ -3,6 +3,6 @@
 
 #include "keemash_mesh_proto.h"
 
-#if KEEMASH_MESH_CORE_VERSION != 0x00050000UL
-#error "firmware requires keemash_mesh_core API 0.5.0"
+#if KEEMASH_MESH_CORE_VERSION < 0x00090400UL
+#error "firmware requires keemash_mesh_core 0.9.4 or newer"
 #endif
